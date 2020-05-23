5c801a3e4b4a869696cc78ff331f3314a6a0da3b
htbkoo <h1017760@connect.hku.hk>
2020-05-23 16:23:06 +0800

    added more test coverage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
-import {render} from '@testing-library/react';
+import {fireEvent, render,} from '@testing-library/react';
 import App, {formatCount} from './App';
 
 describe('<App/>', () => {
     describe('render / component tests', () => {
         test('renders initial time count', () => {
             const {getByText} = render(<App />);
-            const timerElement = getByText(/00:00/i);
+            const timerElement = getByText(/00:000/i);
             expect(timerElement).toBeInTheDocument();
         });
+
+        it("should not add count when not started even if time lapse", () => {
+            // given
+            jest.useFakeTimers();
+            const {getByText} = render(<App />);
+
+            // when
+            jest.advanceTimersByTime(1000);
+
+            // then
+            const timerElement = getByText(/00:000/i);
+            expect(timerElement).toBeInTheDocument();
+        });
+
+        // TODO: fix `Warning: An update to Chronometer inside a test was not wrapped in act(...).`
+        it('should add count to timer after clicked start', () => {
+            // given
+            jest.useFakeTimers();
+            const {getByText} = render(<App />);
+            fireEvent.click(getByText(/Start/i))
+
+            // when
+            jest.advanceTimersByTime(1000);
+
+            // then
+            expect(getByText(/01:000/i)).toBeInTheDocument();
+        });
+
     });
 
     describe('formatCount', () => {
