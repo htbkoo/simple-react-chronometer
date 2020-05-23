613e8dfac81b37b440e09d70a3a52ab1affc0269
htbkoo <h1017760@connect.hku.hk>
2020-05-23 14:15:28 +0800

    grouped test by test suite

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 describe('<App/>', () => {
-    it('renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<App/>, div);
-        ReactDOM.unmountComponentAtNode(div);
+    describe('render / component tests', () => {
+        it('renders without crashing', () => {
+            const div = document.createElement('div');
+            ReactDOM.render(<App />, div);
+            ReactDOM.unmountComponentAtNode(div);
+        });
     });
-
-
 });
