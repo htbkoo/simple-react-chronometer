73df7c92624e41e221b423243cfc241f37c65394
htbkoo <h1017760@connect.hku.hk>
2020-06-03 18:06:26 +0800

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -18,9 +18,38 @@ interface ChronometerState {
 
 const INTERVAL = 10;
 
+interface DateFormat {
+    hours?: number;
+    minutes?: number;
+    seconds: number;
+    milliseconds: number;
+}
+
+function padZero(input: string | number, maxLength: number): string {
+    return input.toString().padStart(maxLength, "0");
+}
+
+function dateFormatToString({hours, minutes, seconds, milliseconds}: DateFormat): string {
+    return [
+        {value: hours},
+        {value: minutes, maxLength: 2},
+        {value: seconds, maxLength: 2},
+        {value: milliseconds, maxLength: 3},
+    ]
+        .filter(element => typeof element.value !== "undefined")
+        .map(({value, maxLength}) =>
+            maxLength
+                ? padZero(value, maxLength)
+                : value.toString()
+        )
+        .join(" : ");
+}
+
 function Chronometer() {
     // const [appState, setAppState] = useState<ChronometerState>({app: {isCounting: false, count: 0}});
-    const [count, setCount] = useState(0);
+    // const [initialValue, setInitialValue] = useState(0);
+    const [initialValue, setInitialValue] = useState(25 * 60 * 1000);
+    const [count, setCount] = useState(initialValue);
     const [counterId, setCounterId] = useState<NodeJS.Timeout | undefined>(undefined);
 
     useEffect(cleanUpCounterOnUnmount, [counterId]);
@@ -78,7 +107,7 @@ function Chronometer() {
                     Stop
                 </button>
                 <button onClick={() => {
-                    setCount(0);
+                    setCount(initialValue);
                 }}>
                     Reset
                 </button>
@@ -99,10 +128,10 @@ function Chronometer() {
 }
 
 export function formatCount(count: number): string {
-
-    const seconds = (Math.floor(count / 1000)).toString().padStart(2, "0");
-    const milliseconds = (count % 1000).toString().padStart(3, "0");
-    return `${seconds}:${milliseconds}`;
+    const minutes = Math.floor(count / 60 / 1000);
+    const seconds = Math.floor(count / 1000) % 60;
+    const milliseconds = count % 1000;
+    return dateFormatToString({minutes, seconds, milliseconds})
 }
 
 export default App;
